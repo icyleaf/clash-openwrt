4de686860591f5e0b63b180f9ac8c79e8da3bec3
Tong Zhao <tzhao11@illinois.edu>
2019-05-04 18:08:21 +0800

    fix log

--- luci-app-clash/luasrc/model/cbi/clash/log.lua
+++ luci-app-clash/luasrc/model/cbi/clash/log.lua
@@ -7,7 +7,7 @@ t = f:field(TextValue, "conf")
 t.rmempty = true
 t.rows = 20
 function t.cfgvalue()
-  luci.sys.exec("[ -f /tmp/clash.log ] && sed '1!G;h;$!d' /tmp/clash.log > /tmp/clash.log")
+  luci.sys.exec("logread | grep clash > /tmp/clash.log")
 	return fs.readfile(conffile) or ""
 end
 t.readonly="readonly"
